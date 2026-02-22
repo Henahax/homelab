952f9313a4fcd03d5f8964ff14d4dae4d42ff68f
Henahax <your_private_email@users.noreply.github.com>
2026-02-22 15:15:56 +0100

    up

--- services/new-container.sh
+++ services/new-container.sh
@@ -1,3 +1,9 @@
 # New ZFS Dataset for Container
 # on ProxMox Host
 zfs create rpool/container-volumes/new-container
+
+# test ?
+chown -R root:root /rpool/container-volumes/
+chmod -R 755 /rpool/container-volumes/
+
+# remove volumes in portainer from previous attempts
